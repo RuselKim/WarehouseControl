5dcd255bd70b519fc8c5801a21a4c7dd33f8836e
RuselKim <sus.metalist@gmail.com>
2017-08-23 00:10:33 +0300

    Create GUI

--- app/src/main/java/com/example/sus/warehousecontrol/MainActivity.java
+++ app/src/main/java/com/example/sus/warehousecontrol/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.sus.warehousecontrol;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    LinearLayout mainLayout;
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
 }
